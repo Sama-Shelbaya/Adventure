b9e838bfed6c338224478385bef79503f31678fc
Sama-Shelbaya <121192155+Sama-Shelbaya@users.noreply.github.com>
2023-03-20 02:25:05 +0200

    submitting Game

---
+++ Adventure/Assets/Scripts/Finish.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour
+{
+    private AudioSource levelupSoundEffect;
+    private bool levelCompletted = false;
+    // Start is called before the first frame update
+   private void Start()
+    {
+        levelupSoundEffect = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name=="player" && !levelCompletted)
+        {
+            levelupSoundEffect.Play ();
+            levelCompletted = true;
+            Invoke("completeLevel", 2f);
+            
+        }
+    }
+
+    private void completeLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Adventure/Assets/Scripts/WayPointFollower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointFollower : MonoBehaviour
+{
+    [SerializeField] private GameObject[] wayPoints;
+    private int CurrentWayPointIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    private void Update()
+    {
+        if (Vector2.Distance(wayPoints[CurrentWayPointIndex].transform.position,transform.position)<.1f)
+        {
+            CurrentWayPointIndex++;
+            if(CurrentWayPointIndex>=wayPoints.Length)
+            {
+                CurrentWayPointIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, wayPoints[CurrentWayPointIndex].transform.position, Time.deltaTime * speed);
+    }
+}
